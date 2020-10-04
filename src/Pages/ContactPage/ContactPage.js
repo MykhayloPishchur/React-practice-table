0797fc155a4dd8cd836696a33e5e8e4c2677dd15
unknown <you@example.com>
2020-10-04 17:59:26 +0300

    fix

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -30,6 +30,17 @@ class contactWiew extends Component {
 
     return (
       <div className={style.container}>
+        <div className={style.selectContainer}>
+          <div className={style.selectWrap}>
+            <span>Per page :</span>
+            <select className="custom-select">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
         <TableWiew users={currentPosts}></TableWiew>
         <Pagination
           postsPerPage={postsPerPage}
